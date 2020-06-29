55cc8afad0eef341f7a94f5a1424e32b50366865
faberv-cmd <fabrizioveneziano@yahoo.it>
2020-06-29 10:27:11 +0200

    Fix DB

--- db/script_new_tables.sql
+++ db/script_new_tables.sql
@@ -19,7 +19,7 @@ CREATE TABLE products (
   description varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   availability int(11) NOT NULL,
   price double(10,2) NOT NULL,
-  availability varchar(25) NOT NULL,
+  category varchar(50) not null,
   PRIMARY KEY (prod_id)
 );
 
@@ -28,12 +28,12 @@ CREATE TABLE shopping_lists (
   user_id int(11) NOT NULL,
   total_price double(10,2) NOT NULL,
   shopping_list JSON NOT NULL,
-  datetime DEFAULT   CURRENT_TIMESTAMP,
+  shopping_list_date datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (shopping_list_id)
 );
 
 INSERT INTO users VALUES (1,'admin','admin','admin','Mario','Rossi','Via x'),(2,'user','user','user','Mario','Gialli', 'Piazza y');
 
-INSERT INTO products VALUES (1, "sedia", "ha 4 piedi", 10, 3,20,"mobili"), (2, "tavolo", "ha 4 piedi", 10, 7,20, "mobili"), (3, "martello", "pesa poco", 40, 5,20, "strumenti"),(4, "cacciavite", "è comodo", 40, 2,20,"strumenti") ;
+INSERT INTO products VALUES (1, "sedia", "ha 4 piedi", 10, 3.20,"mobili"), (2, "tavolo", "ha 4 piedi", 10, 7.20, "mobili"), (3, "martello", "pesa poco", 40, 5.20, "strumenti"),(4, "cacciavite", "è comodo", 40, 2.20,"strumenti") ;
 
-INSERT INTO shopping_lists VALUES (1, 2, 11,80, '{"1": 2, "3": 2}');
+INSERT INTO shopping_lists VALUES (1, 33, 11.80, '{"1": 2, "3": 2}', now());
