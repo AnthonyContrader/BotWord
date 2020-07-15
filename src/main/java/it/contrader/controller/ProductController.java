d8ddc1f92f4fcfe674be408b536d999c4790df0d
faberv-cmd <fabrizioveneziano@yahoo.it>
2020-07-15 11:36:10 +0200

    Fix requestParam

--- src/main/java/it/contrader/controller/ProductController.java
+++ src/main/java/it/contrader/controller/ProductController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.contrader.dto.ProductDTO;
@@ -21,8 +21,8 @@ public class ProductController extends AbstractController<ProductDTO> {
 	@Autowired
 	private ProductService prodService;
 	
-	@GetMapping(value="/getForCategory/{category}")
-	public List<ProductDTO> getProductsForCategory(@PathVariable(value = "category") String category){
+	@GetMapping(value="/getForCategory")
+	public List<ProductDTO> getProductsForCategory(@RequestParam("category") String category){
 		return prodService.findByCategory(category);
 	}
 
--- src/main/java/it/contrader/controller/ShoppingListController.java
+++ src/main/java/it/contrader/controller/ShoppingListController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.contrader.dto.ShoppingListDTO;
@@ -20,8 +20,8 @@ public class ShoppingListController extends AbstractController<ShoppingListDTO> 
 	@Autowired
 	private ShoppingListService slService;
 	
-	@GetMapping(value="/getbyuserid/{userId}")
-	public List<ShoppingListDTO> getShopListByUserId(@PathVariable(value = "userId") Long userId){
+	@GetMapping(value="/getbyuserid")
+	public List<ShoppingListDTO> getShopListByUserId(@RequestParam("userId") Long userId){
 		return slService.findByUserId(userId);
 		
 	}
